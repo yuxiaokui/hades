07f90627edfae330e4e4e233363b320940c89ba3
小葵 <xi4okui@gmail.com>
2022-04-07 09:58:42 +0800

    Delete test.py

--- pocs/test.py
+++
@@ -1,9 +0,0 @@
-def poc(target):
-  if target[-1] != "/"
-    url = target + "/"
-  else:
-    url = target
-    r = requests.get( url + "robots.txt")
-  if "Disallow:" in r.text:
-    return True
-  
